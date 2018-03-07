0806ae9837f85ddc892c4d9e6f103c2183237db3
Christian Reichel <chrstn.rchl@gmail.com>
2018-03-07 14:36:50 +0100

    Update to newest face extractor version.

--- face_trainer.py
+++ face_trainer.py
@@ -1,3 +1,14 @@
+#######################################################
+# Face trainer - face extractor application
+# @author: Christian Reichel
+# Version: 0.1a
+# -----------------------------------------------------
+# This script makes a directory based on an entered
+# name and a hash value and saves X faces into that
+# folder as training set for face recognition.
+#######################################################
+
+# IMPORTS
 from modules.face_extractor import face_extractor as extractor
 from modules.face_detector import face_detector as detector
 
