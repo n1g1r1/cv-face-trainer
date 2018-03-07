fc40975e30ed7171710e18d1c7e53948c8fb1bb3
Christian Reichel <chrstn.rchl@gmail.com>
2018-03-07 16:31:18 +0100

    Update face detection classifier.

--- face_trainer.py
+++ face_trainer.py
@@ -12,4 +12,4 @@
 from modules.face_extractor import face_extractor as extractor
 from modules.face_detector import face_detector as detector
 
-extractor.build_training_set(detector)
+extractor.build_training_set(detector, classifier = "haar")
